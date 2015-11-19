542ce3069bf0a390d3a600ecd0cf06292f0cf62c
Ada Ji <adajixyz@gmail.com>
2015-11-20 06:33:43 +0800

    Add MBProgressHUD

---
+++ OnTheMap/OnTheMap-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  OnTheMap-Bridging-Header.h
+//  OnTheMap
+//
+//  Created by Ada Ji on 11/20/15.
+//  Copyright © 2015 Ada Ji. All rights reserved.
+//
+
+#ifndef OnTheMap_Bridging_Header_h
+#define OnTheMap_Bridging_Header_h
+
+#import <MBProgressHUD/MBProgressHUD.h>
+
+#endif /* OnTheMap_Bridging_Header_h */
