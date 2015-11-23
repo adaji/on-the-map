72219fd5e91c7af3034afafc18afd5778623d379
Ada Ji <adajixyz@gmail.com>
2015-11-23 14:55:37 +0800

    Minor changes to custom text field

--- OnTheMap/CustomTextField.swift
+++ OnTheMap/CustomTextField.swift
@@ -32,12 +32,10 @@ class CustomTextField: UITextField {
         leftViewMode = .Always
         
         backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.65)
-        // TODO: Change font to SF Regular
         let darkerColor = UIColor(red: 238/255.0, green:62/255.0, blue:10/255.0, alpha: 1.0)
         textColor = darkerColor
         tintColor = darkerColor
-        font = UIFont(name: "SF UI Text Medium", size: 17.0)
-        // TODO: Change placeholder font to SF Regular
+        font = UIFont.systemFontOfSize(17.0)
         attributedPlaceholder = NSAttributedString(string: self.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.systemFontOfSize(17)])
     }
 
